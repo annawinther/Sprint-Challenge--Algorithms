8ef7f034e7525571cde783ad05140abbfed098a3
Anna Winther <anski.anna@gmail.com>
2020-02-21 11:06:36 +0000

    Working and passing count function and update psuedo code after debugging

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,12 +4,17 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    # base case if the lenght of the word is less than 2 
+     # base case if the lenght of the word is less than 2 
+    if len(word) < 2:
         # return 0 
-    # check if the left side of the word contains 'th'
-        # if so, return 1 + the right side of the work where the 'th' is
+        return 0
+    # check if the word after taken away the first two characters'th'
+    if word[:2] == "th":
+        # if so, return 1 + the right side of the work where the 'th' is. 
+        return 1 + count_th(word[2:])
     # otherwise 
+    else:
         # return the other side of the word not containing 'th'
-    pass
+        return 0 + count_th(word[1:])
 
-print(count_th("svhthhellothis"))
+print(count_th("hellothere"))
