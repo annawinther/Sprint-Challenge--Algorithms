97e4475ab7dfb985e737a8afe2937f3ac1a3a525
Anna Winther <anski.anna@gmail.com>
2020-02-21 10:53:19 +0000

    Count_th psuedo coded

--- recursive_count_th/count_th.py
+++ recursive_count_th/count_th.py
@@ -4,7 +4,12 @@ Your function should return a count of how many occurences of ***"th"*** occur w
 Your function must utilize recursion. It cannot contain any loops.
 '''
 def count_th(word):
-    
-    # TBC
-    
+    # base case if the lenght of the word is less than 2 
+        # return 0 
+    # check if the left side of the word contains 'th'
+        # if so, return 1 + the right side of the work where the 'th' is
+    # otherwise 
+        # return the other side of the word not containing 'th'
     pass
+
+print(count_th("svhthhellothis"))
